8bfcbd9aca635b45e9876b2524671209926f6933
higorcos <higorcosta2001@outlook.com>
2026-07-15 19:43:49 -0300

    Update Footer component styles for improved alignment and readability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,11 @@
 export default function Footer() {
   return (
-    <div className="container mx-auto flex max-w-4xl p-4 text-sm">
-      <p className="flex-grow">
+    <div className="container mx-auto flex max-w-4xl p-4 text-sm center">
+      <p className="flex-grow center">
         &copy; {new Date().getFullYear()} Higor Costa &middot; Todos os direitos
         reservados.
       </p>
-      <a
-        href="https://github.com/higorcos/higorcos"
-        target="_blank"
-        rel="noreferrer"
-        className="text-gray-500 hover:text-gray-900"
-      >
-        Código-fonte
-      </a>
+      
     </div>
   )
 }
