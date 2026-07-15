6a9ed42afef1531d80abdaf398f9c6583d5d680e
higorcos <higorcosta2001@outlook.com>
2026-07-15 19:44:51 -0300

    Update Footer component styles for improved layout and readability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
 export default function Footer() {
   return (
-    <div className="container mx-auto flex max-w-4xl p-4 text-sm center">
-      <p className="flex-grow center">
-        &copy; {new Date().getFullYear()} Higor Costa &middot; Todos os direitos
-        reservados.
-      </p>
-      
-    </div>
+   <div className="container mx-auto max-w-4xl p-2 text-sm">
+  <p className="text-center">
+    &copy; {new Date().getFullYear()} Higor Costa &middot; Todos os direitos
+    reservados.
+  </p>
+</div>
   )
 }
